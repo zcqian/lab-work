4f63e2590ceafb41983398a9082967f63d581c58
Zhongchao Qian <squid157@gmail.com>
2019-05-20 14:02:50 -0400

    added cifar10/100 datasets

--- model-eval/datasets.py
+++ model-eval/datasets.py
@@ -1,6 +1,6 @@
 import os
 import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
+from torchvision.datasets import ImageFolder, CIFAR10, CIFAR100
 
 
 def imagenet1k():
@@ -44,4 +44,35 @@ def tinyimagenet():
         transforms.ToTensor(),
         normalize,
     ]))
+    return dataset_train, dataset_val
+
+
+def cifar100():
+    normalize = transforms.Normalize(mean=(0.5071, 0.4865, 0.4409), std=(0.2673, 0.2564, 0.2762))
+    dataset_train = CIFAR100(root='～/Datasets/cifar100', train=True, transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            normalize,
+        ]), download=True)
+    dataset_val = datasets.CIFAR100(root='~/Datasets/cifar100', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    return dataset_train, dataset_val
+
+
+def cifar10():
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    dataset_train = CIFAR10(root='～/Datasets/cifar10', train=True, transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            normalize,
+        ]), download=True)
+    dataset_val = datasets.CIFAR10(root='~/Datasets/cifar10', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ]))
     return dataset_train, dataset_val
