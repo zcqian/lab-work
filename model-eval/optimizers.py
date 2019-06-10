c60065b0af8d1a240a5e335283904fa8b8bab716
Zhongchao Qian <squid157@gmail.com>
2019-06-10 17:29:06 -0400

    Add back the different weight decay one

--- model-eval/optimizers.py
+++ model-eval/optimizers.py
@@ -14,12 +14,15 @@ def resnet_imagenet_sgd_default(model: torch.nn.Module):
     return optimizer, scheduler
 
 
-def resnet_cifar_sgd(model, start_epoch=0):
+def resnet_cifar_sgd(model, wd=1e-4):
     LR = 0.1
     MOMENTUM = 0.9
-    WD = 1e-4
     optimizer = torch.optim.SGD(
-        model.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=WD
+        model.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=wd
     )
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [100, 150])
     return optimizer, scheduler
+
+
+def resnet_cifar_sgd_wd5(model):
+    return resnet_cifar_sgd(model, wd=5e-4)
