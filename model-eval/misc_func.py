e8edd6161cf0698f3012e2bb3a320af7d1f9e3d1
Zhongchao Qian <squid157@gmail.com>
2019-07-22 13:51:23 -0400

    add CUDA option

--- model-eval/misc_func.py
+++ model-eval/misc_func.py
@@ -9,22 +9,28 @@ from main import accuracy
 from main import AverageMeter, ProgressMeter
 
 
-def validate_10crop(val_loader: DataLoader, model: nn.Module):
+def validate_10crop(val_loader: DataLoader, model: nn.Module, cuda=False):
     batch_time = AverageMeter('Time', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
                              prefix='Test: ')
-
+    
     # we use only this criterion at the moment 
     criterion = nn.CrossEntropyLoss()
+    # CUDA if used
+    if cuda:
+        model = model.cuda()
+        criterion = criterion.cuda()
     # switch to evaluate mode
     model.eval()
 
     with torch.no_grad():
         end = time.time()
         for i, (input, target) in enumerate(val_loader):
+            if cuda:
+                input, target = input.cuda(non_blocking=True), target.cuda(non_blocking=True)
             # this assumes input is ncrop
             bs, ncrops, c, h, w = input.size()
             # compute output
