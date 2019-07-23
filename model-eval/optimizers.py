4080c7d40a5d9e08a99de357069440671119e0eb
Zhongchao Qian <squid157@gmail.com>
2019-07-23 11:06:36 -0400

    added fine tune optim which uses very few epochs

--- model-eval/optimizers.py
+++ model-eval/optimizers.py
@@ -14,6 +14,17 @@ def resnet_imagenet_sgd_default(model: torch.nn.Module):
     return optimizer, scheduler
 
 
+def resnet_imagenet_sgd_tune(model: torch.nn.Module):
+    LR = 0.1
+    MOMENTUM = 0.9
+    WD = 1e-4
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=LR, momentum=MOMENTUM, weight_decay=WD
+    )
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [5, 10])
+    return optimizer, scheduler
+
+
 def resnet_cifar_sgd(model, wd=1e-4):
     LR = 0.1
     MOMENTUM = 0.9
