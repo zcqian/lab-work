47c3d5da5c86d7c7f84d77e0771929641a48ff8e
Zhongchao Qian <zcqian@zcqian.net>
2019-10-14 14:23:36 -0400

    basic linear lr decay

---
+++ model-eval/lr_scheduler.py
@@ -0,0 +1,10 @@
+from torch.optim.lr_scheduler import _LRScheduler
+
+
+class LinearDecayLR(_LRScheduler):
+    def __init__(self, optimizer, slope, last_epoch=-1):
+        self.slope = slope
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [base_lr + self.slope * self.last_epoch for base_lr in self.base_lrs]
